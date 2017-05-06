7acdc89500338e7820d0000a78d23cb67e497d4d
Mélioratif <elekis@cmaj9.com>
2017-05-06 19:24:40 +0200

    refactoring all the codes

---
+++ js/constellation.js
@@ -0,0 +1,127 @@
+
+
+//function CONSTELLATIONS
+
+var constellationsArray = {};
+
+
+function initConstellationData()
+{        
+        for (var i = 0; i < RAW_DATA.allStarsArray().length; i++){
+                if( !(RAW_DATA.allStarsArray()[i][h_con] in constellationsArray )) { 
+                        if(RAW_DATA.allStarsArray()[i][h_con]  == null) continue;
+                        constellationsArray[RAW_DATA.allStarsArray()[i][h_con]] = new constellation(RAW_DATA.allStarsArray()[i][h_con]);
+                }
+                constellationsArray[RAW_DATA.allStarsArray()[i][h_con]].all_stars.push(RAW_DATA.allStarsArray()[i]);
+        }
+
+        for (var p = 0; p < fo_t_path.length; p++)
+          {
+                var aStar = fo_t_path[p];
+                constellationsArray[aStar[0]].fullname = aStar[1];
+
+                for (var i = 2; i < aStar.length; i++)
+                    {
+                        var a = RAW_DATA.allStarsArray().filter(function(val) { return val[h_hip] == aStar[i];});
+                        if (a.length != 1)
+                                throw "findAStars("+aStar[0]+"="+aStar[i]+") : a.length is different of 1. ("+ a +")"+"("+a.length+")";
+ 
+                       constellationsArray[aStar[0]].cst_stars.push(a[0]);
+                    }
+          }
+
+        for (var p = 0; p < fo_t_path.length; p++)
+        {
+                var aConstell = fo_t_path[p];
+
+                var c = constellationsArray[aConstell[0]];
+                for (var i = 3; i < aConstell.length; i++) // 3 because, the 2 first is the name and the shortname
+                {
+                   var from = c.cst_stars.filter(function(val) { return val[h_hip] == aConstell[i-1];});
+                   var to =c.cst_stars.filter(function(val) { return val[h_hip] == aConstell[i];});
+                
+//                   if (from.length != 1){
+ //                       console.log(aConstell[0]);
+ //                       console.log(from);
+  //                   }
+                    c.cst_connect.push([from[0],to[0]]);
+                }
+        }
+}
+
+function constellation(name){
+        this.fullname = "";
+        this.shortname = name;
+        this.farest_stars = 0;
+        this.middle_stars = 0;
+        this.all_stars = [];
+        this.cst_stars = [];
+        this.cst_connect = [];
+
+
+        this.resetConnexionColor = function()
+        {
+         this.setToColor(new BABYLON.Color3(1,1 , 1));
+       }
+
+        this.ConnexionToColor = function()
+        {
+         this.setToColor(new BABYLON.Color3(1,0 , 0));
+       }
+
+
+        this.setToColor = function(color)
+        {
+         for (var i = 0; i < this.cst_connect.length;i++)
+                {
+                      
+                      var line = this.cst_connect[i][2];
+                      line.color = color
+                }
+        }
+
+
+        this.find=function(hipnb)
+        {
+        var a = this.all_stars.filter(function(val) { return val[h_hip] ==hipnb;});
+        if (a.length != 1)
+                throw "findAStars("+hipnb+") : a.length is different of 1. ("+ a +")"+"("+a.length+")";
+        return a[0];
+        }
+
+        this.drawConstellationStars = function(scene)
+        {
+            for (var p = 0; p < this.cst_stars.length; p++){                     
+                   var stars_info = this.cst_stars[p]; 
+                   var light0 = new BABYLON.PointLight("LI:"+stars_info[h_hip], new BABYLON.Vector3(stars_info[h_x_rel],stars_info[h_y_rel],stars_info[h_z_rel]), scene);
+
+            
+                    // Creating light sphere
+                    // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
+                    var lightSphere0 = BABYLON.Mesh.CreateSphere("SP:"+stars_info[h_hip]+":"+stars_info[h_con], 10, 30, scene);
+                    
+                    lightSphere0.material = new BABYLON.StandardMaterial("SM:"+stars_info[h_hip], scene);
+                    lightSphere0.material.diffuseColor = new BABYLON.Color3(0.5,0.7 , 0);
+                    lightSphere0.material.specularColor = new BABYLON.Color3(1, 0, 0);
+                    lightSphere0.material.emissiveColor = new BABYLON.Color3(1, 0.5, 1);
+
+                    lightSphere0.position = light0.position;    
+                 }
+        }
+
+        this.drawConstellationConnexion = function(scene)
+        {
+        for (var i = 0; i < this.cst_connect.length; i++){
+                var from = this.cst_connect[i][0];
+                var to = this.cst_connect[i][1];
+                var lines = BABYLON.Mesh.CreateLines("lines", [
+                    new BABYLON.Vector3(from[h_x_rel], from[h_y_rel], from[h_z_rel]),
+                    new BABYLON.Vector3(to[h_x_rel], to[h_y_rel], to[h_z_rel]),
+                ], scene);   
+//                console.log(lines);     
+                this.cst_connect[i].push(lines);
+          };
+        }
+
+}
+
---
+++ js/menu.js
@@ -0,0 +1,126 @@
+
+var constructMenu = function()
+{      
+        document.getElementById("buttonview").addEventListener("click", function() {myF("viewDrop");}, false);
+        document.getElementById("buttonselect").addEventListener("click", function() {myF("selectDrop");}, false);
+        document.getElementById("myInputview").addEventListener("keyup", function() {filterFunction('viewDrop','myInputview');});
+        document.getElementById("myInputselect").addEventListener("keyup", function() {filterFunction('selectDrop','myInputselect');});
+
+
+//            <input type="checkbox" name="all" value=All class="dropdown-content">All<br>
+        var mydropdwn = document.getElementById("selectDrop");
+        var divelem = document.createElement("div");
+        mydropdwn.appendChild(divelem);               
+        var inputelem = document.createElement("input");
+        inputelem.setAttribute("type","checkbox");
+        inputelem.setAttribute("value","All");
+        divelem.appendChild(inputelem);
+        var txtelem = document.createTextNode("All");
+        divelem.appendChild(txtelem);
+        divelem.className = "dropbtn";
+
+//    <input type="checkbox" name="none" value="None" class="dropdown-content">None<br>
+        var divelem = document.createElement("div");
+        mydropdwn.appendChild(divelem);               
+        var inputelem = document.createElement("input");
+        inputelem.setAttribute("type","checkbox");
+        inputelem.setAttribute("value","None");
+        divelem.appendChild(inputelem);
+        var txtelem = document.createTextNode("None");
+        divelem.appendChild(txtelem);
+        divelem.className = "dropbtn";
+
+        var short = [];
+        for(key in constellationsArray){short.push(key);}
+        short = short.sort();
+        for(i in short)
+        {
+                var constel = constellationsArray[short[i]];
+                var divelem = document.createElement("div");
+                mydropdwn.appendChild(divelem);               
+                var inputelem = document.createElement("input");
+                inputelem.setAttribute("type","checkbox");
+                inputelem.setAttribute("value",constel.shortname);
+                divelem.appendChild(inputelem);
+                var txtelem = document.createTextNode(constel.fullname+"("+constel.cst_stars.length+")");
+                divelem.appendChild(txtelem);
+                divelem.className = "dropbtn";
+
+                divelem.appendChild(document.createElement("br"));
+        }
+
+        var mydropdwn = document.getElementById("viewDrop");
+        for(i in short)
+        {
+                var constel = constellationsArray[short[i]];
+                var divelem = document.createElement("div");
+                mydropdwn.appendChild(divelem);           
+                        
+                // <button id="buttonview" class="dropbtn">View</button>
+                var btelem = document.createElement("button");
+                btelem.type = "button";
+                btelem.className = "dropbtn";
+                btelem.textContent = constel.fullname+"("+constel.cst_stars.length+")";
+                btelem.onclick = function(j) { return function() { cameraOn(j); }; }(constel.shortname);
+
+                
+                divelem.appendChild(btelem);
+                divelem.appendChild(document.createElement("br"));
+        }
+}
+
+function myF(mId) {
+            document.getElementById(mId).classList.toggle("show");
+        }
+
+function filterFunction(idm,inputid) {
+            var input = document.getElementById(inputid);
+            var filter = input.value.toUpperCase();
+
+            var div = document.getElementById(idm);
+            var a = div.getElementsByTagName("div");
+            for (var i = 0; i < a.length; i++) {
+                if (a[i].textContent.toUpperCase().indexOf(filter) > -1) {
+                    a[i].style.display = "";
+                } else {
+                    a[i].style.display = "none";
+                }
+            }
+        }
+
+
+var cameraOn=function(cstname)
+        {
+                stars = constellationsArray[cstname].cst_stars;
+                var x=0,y=0,z=0;        
+                for(var i = 0; i < stars.length; i++)
+                {       
+                        x+= stars[i][h_x_rel];
+                        y+= stars[i][h_y_rel];
+                        z+= stars[i][h_z_rel];
+                }
+                x= x/stars.length;
+                y= y/stars.length;
+                z= z/stars.length;
+                
+                
+                getCurrentScene().activeCamera.setTarget(new BABYLON.Vector3(x,y,z));
+
+                for (key in  constellationsArray){
+                      var cons = constellationsArray[key];
+                      cons.resetConnexionColor();
+                   }       
+                
+                constellationsArray[cstname].ConnexionToColor();
+                }
+
+
+
+
+
+
+
+
+
+
+
